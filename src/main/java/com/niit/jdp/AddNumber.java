1b556628033fb03b28f22064468b4410abe1a061
pranay <pranayanand61@gmail.com>
2023-05-24 05:25:13 +0530

    initialize a for loop

--- src/main/java/com/niit/jdp/AddNumber.java
+++ src/main/java/com/niit/jdp/AddNumber.java
@@ -5,5 +5,11 @@ public class AddNumber {
         // initialize a variable sum
         int sum = 0;
 
+        // initialize a for loop
+        for (int number = 1; number <= 100 ; number++) {
+
+
+        }
+
     }
 }
